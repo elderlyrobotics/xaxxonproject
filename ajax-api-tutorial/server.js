a8d20a7f775c2fbb26e67a6ce8c684b1efaccdb9
elderlyrobotics <elderlyrobotics@gmail.com>
2017-06-29 14:18:33 -0700

    finished AJAX RESTful API tutorial

--- ajax-api-tutorial/server.js
+++ ajax-api-tutorial/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var products = [
 {
@@ -17,10 +18,55 @@ var currentId = 2;
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
 
 app.get('/products', function(req, res) {
 	res.send({ products: products });
 });
+
+
+app.post('/products', function(req, res) {
+    var productName = req.body.name;
+    currentId++;
+
+    products.push({
+        id: currentId,
+        name: productName
+    });
+
+    res.send('Successfully created product!');
+});
+
+app.put('/products/:id', function(req,res) {
+	var id = req.params.id;
+	var newName = req.body.newName;
+
+	var found = false;
+
+	products.forEach(function(product,index) {
+		if (!found && product.id === Number(id)) {
+			product.name = newName;
+		}
+	});
+
+	res.send('Successfully updated product!');
+});
+
+app.delete('/products/:id', function(req, res) {
+	var id = req.params.id;
+
+	var found = false;
+
+	products.forEach(function(product, index) {
+		if (!found && product.id === Number(id)) {
+			products.splice(index, 1);
+	    }
+	});
+	
+	res.send('Successfully deleted product!');
+});
+
 
 app.listen(PORT, function() {
 	console.log('Server listening on ' + PORT);
